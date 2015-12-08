a8beefd876f706b49d9fa1781022829965722b10
Beni <beni.buess@nzz.ch>
2015-12-08 11:24:04 +0100

    do not show overlapping labels for date type on x axis, WIP

--- dist/amd/resources/seriesTypes.js
+++ dist/amd/resources/seriesTypes.js
@@ -12,46 +12,111 @@ define(['exports'], function (exports) {
     return value;
   }
 
-  var dateFormatForPrecisions = {
-    year: function year(index, date) {
-      if (index === 0) {
-        return date.getFullYear();
-      } else {
-        return date.getFullYear().toString().slice(2);
+  var dateSettingsForPrecisions = {
+    year: {
+      format: function format(index, date) {
+        if (index === 0) {
+          return date.getFullYear();
+        } else {
+          return date.getFullYear().toString().slice(2);
+        }
+      },
+      getLabelLength: function getLabelLength(index) {
+        return index === 0 ? 100 : 20;
       }
     },
-    month: function month(index, date) {
-      if (index === 0) {
-        return pad(date.getMonth() + 1, 2) + '.' + date.getFullYear();
-      } else {
-        return '' + pad(date.getMonth() + 1, 2);
+    month: {
+      format: function format(index, date) {
+        if (index === 0) {
+          return pad(date.getMonth() + 1, 2) + '.' + date.getFullYear();
+        } else {
+          return '' + pad(date.getMonth() + 1, 2);
+        }
+      },
+      getLabelLength: function getLabelLength(index) {
+        return index === 0 ? 100 : 20;
       }
     },
-    day: function day(index, date) {
-      if (index === 0) {
-        return pad(date.getDate(), 2) + '.' + pad(date.getMonth() + 1, 2) + '.' + date.getFullYear();
-      } else {
-        return '' + pad(date.getDate(), 2);
+    day: {
+      format: function format(index, date) {
+        if (index === 0) {
+          return pad(date.getDate(), 2) + '.' + pad(date.getMonth() + 1, 2) + '.' + date.getFullYear();
+        } else {
+          return '' + pad(date.getDate(), 2);
+        }
+      },
+      getLabelLength: function getLabelLength(index) {
+        return index === 0 ? 100 : 20;
+      }
+    },
+    hour: {
+      format: function format(index, date) {
+        return pad(date.getHours() + 1, 2) + ':' + pad(date.getMinutes(), 2);
+      },
+      getLabelLength: function getLabelLength(index) {
+        return 40;
       }
     }
   };
 
+  function modifyConfigDateX(config, typeOptions, data, size, rect) {
+    config.axisX = config.axisX || {};
+
+    var labels = data.labels.map(function (label, index) {
+      return {
+        space: dateSettingsForPrecisions[typeOptions.precision].getLabelLength(index)
+      };
+    });
+
+    var xAxisWidth = rect.width - ((config.axisY.offset || 0) + 10);
+
+    var enoughSpace = labels.reduce(function (sum, label) {
+      return sum + label.space;
+    }, 0) < xAxisWidth;
+
+    if (enoughSpace) {
+
+      labels.map(function (label) {
+        return label.show = true;
+      });
+    } else {
+      (function () {
+
+        var numberOfLabels = data.labels.length;
+        var spacePerLabel = xAxisWidth / labels.length;
+
+        labels.map(function (label, index) {
+          var spaceNeededBefore = 0;
+          var i = index;
+          while (i--) {
+            if (labels[i]) {
+              spaceNeededBefore = spaceNeededBefore + (labels[i].show ? labels[i].space : 0);
+            }
+          }
+          if (spaceNeededBefore < (index + 1) * spacePerLabel) {
+            label.show = true;
+          } else {
+            label.show = false;
+          }
+        });
+      })();
+    }
+
+    config.axisX.labelInterpolationFnc = function (value, index) {
+      if (dateSettingsForPrecisions.hasOwnProperty(typeOptions.precision)) {
+        value = dateSettingsForPrecisions[typeOptions.precision].format(index, new Date(value));
+      }
+
+      if (labels[index].show) {
+        return value;
+      }
+    };
+  }
+
   var seriesTypes = {
     'date': {
       'x': {
-        modifyConfig: function modifyConfig(config, typeOptions, data, size, rect) {
-
-          config.axisX = config.axisX || {};
-          config.axisX.labelInterpolationFnc = function (value, index) {
-            var xAxisWidth = rect.width - ((config.axisY.offset || 0) + 10);
-
-            if (dateFormatForPrecisions.hasOwnProperty(typeOptions.precision)) {
-              value = dateFormatForPrecisions[typeOptions.precision](index, new Date(value));
-            }
-
-            return value;
-          };
-        }
+        modifyConfig: modifyConfigDateX
       }
     }
   };
--- src/resources/seriesTypes.js
+++ src/resources/seriesTypes.js
@@ -6,46 +6,111 @@ function pad(value, toLength) {
   return value;
 }
 
-var dateFormatForPrecisions = {
-  year: (index, date) => {
-    if (index === 0) {
-      return date.getFullYear();
-    } else {
-      return date.getFullYear().toString().slice(2);
+var dateSettingsForPrecisions = {
+  year: {
+    format: (index, date) => {
+      if (index === 0) {
+        return date.getFullYear();
+      } else {
+        return date.getFullYear().toString().slice(2);
+      }
+    },
+    getLabelLength: (index) => {
+      return index === 0 ? 100 : 20;
     }
   },
-  month: (index, date) => {
-    if (index === 0) {
-      return `${pad(date.getMonth()+1,2)}.${date.getFullYear()}`;
-    } else {
-      return `${pad(date.getMonth()+1,2)}`;
+  month: {
+    format: (index, date) => {
+      if (index === 0) {
+        return `${pad(date.getMonth()+1,2)}.${date.getFullYear()}`;
+      } else {
+        return `${pad(date.getMonth()+1,2)}`;
+      }
+    },
+    getLabelLength: (index) => {
+      return index === 0 ? 100 : 20;
     }
   },
-  day: (index, date) => {
-    if (index === 0) {
-      return `${pad(date.getDate(),2)}.${pad(date.getMonth()+1,2)}.${date.getFullYear()}`;
-    } else {
-      return `${pad(date.getDate(),2)}`;
+  day: {
+    format: (index, date) => {
+      if (index === 0) {
+        return `${pad(date.getDate(),2)}.${pad(date.getMonth()+1,2)}.${date.getFullYear()}`;
+      } else {
+        return `${pad(date.getDate(),2)}`;
+      }
+    },
+    getLabelLength: (index) => {
+      return index === 0 ? 100 : 20;
     }
   },
+  hour: {
+    format: (index, date) => {
+      return `${pad(date.getHours()+1,2)}:${pad(date.getMinutes(),2)}`;
+    },
+    getLabelLength: (index) => {
+      return 40;
+    }
+  }
+}
+
+function modifyConfigDateX(config, typeOptions, data, size, rect) {
+  config.axisX = config.axisX || {};
+
+  var labels = data.labels.map((label, index) => {
+    return {
+      space: dateSettingsForPrecisions[typeOptions.precision].getLabelLength(index)
+    }
+  });
+
+  let xAxisWidth = rect.width - ((config.axisY.offset || 0) + 10);
+
+  // do we have space for all the labels?
+  let enoughSpace = labels.reduce((sum, label) => { return sum + label.space; }, 0) < xAxisWidth;
+
+  if (enoughSpace) {
+
+    labels.map(label => label.show = true);
+
+  } else {
+
+    let numberOfLabels = data.labels.length;
+    let spacePerLabel = xAxisWidth/labels.length;
+
+    // drop labels if they would overlap the label before.
+    // better solution would be: calculate the interval to know in what interval we can drop labels
+    // then do this recursively by multiplying the interval until we have enough space
+    labels.map((label, index) => {
+      let spaceNeededBefore = 0;
+      let i = index;
+      while(i--) {
+        if (labels[i]) {
+          spaceNeededBefore = spaceNeededBefore + (labels[i].show ? labels[i].space : 0);
+        }
+      }
+      if (spaceNeededBefore < (index + 1) * spacePerLabel) {
+        label.show = true;
+      } else {
+        label.show = false;
+      }
+    })
+
+  }
+
+  config.axisX.labelInterpolationFnc = (value, index) => {
+    if (dateSettingsForPrecisions.hasOwnProperty(typeOptions.precision)) {
+      value = dateSettingsForPrecisions[typeOptions.precision].format(index, new Date(value));
+    }
+
+    if (labels[index].show) {
+      return value;
+    }
+  }
 }
 
 export var seriesTypes = {
   'date': {
     'x': {
-      modifyConfig: (config, typeOptions, data, size, rect) => {
-
-        config.axisX = config.axisX || {};
-        config.axisX.labelInterpolationFnc = (value, index) => {
-          let xAxisWidth = rect.width - ((config.axisY.offset || 0) + 10);
-
-          if (dateFormatForPrecisions.hasOwnProperty(typeOptions.precision)) {
-            value = dateFormatForPrecisions[typeOptions.precision](index, new Date(value));
-          }
-
-          return value;
-        }
-      }
+      modifyConfig: modifyConfigDateX
     }
   }
 }
