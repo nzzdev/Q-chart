178807ee16ad962ca117de1c2117e37e7cb94a28
benib <beni.buess@nzz.ch>
2017-12-20 12:11:31 +0100

    fix: return display-options-schema.json if requested

--- routes/schema.js
+++ routes/schema.js
@@ -11,8 +11,8 @@ module.exports = [
   {
     method: 'GET',
     path: '/display-options-schema.json',
-    handler: function(request, reply) {
-      reply.file(resourcesDir + 'display-options-schema.json');
+    handler: function(request, h) {
+      return h.file(resourcesDir + 'display-options-schema.json');
     }
   }
 ];
