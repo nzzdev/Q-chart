5bcb123995291e949569163e544f1c9aba9c7b49
benib <beni.buess@nzz.ch>
2018-02-09 18:19:17 +0100

    take divisor into account to figure out if we need grouping on y axis labels or not

--- chartTypes/line/prerender.js
+++ chartTypes/line/prerender.js
@@ -1,5 +1,5 @@
 const d3config = require("../../config/d3.js");
-const getFlatData = require("../../helpers/data.js").getFlatData;
+const dataHelpers = require("../../helpers/data.js");
 
 const d3 = {
   format: require("d3-format"),
@@ -9,16 +9,18 @@ const d3 = {
 module.exports = function(item, toolRuntimeConfig, spec, vega) {
   // if we have one value >= 10000 in the Y Axis, we will group thousands
   try {
+    const divisor = dataHelpers.getDivisor(item.data);
     const minValue = spec.scales[1].minValue || 0;
     const maxValue =
       spec.scales[1].maxValue ||
-      getFlatData(item.data)
+      dataHelpers
+        .getFlatData(item.data)
         .sort((a, b) => a - b)
         .pop();
 
     const scale = d3.scale
       .scaleLinear()
-      .domain([minValue, maxValue])
+      .domain([minValue / divisor, maxValue / divisor])
       .nice(spec.scales[1].nice); // the nice value of Y Scale
 
     // at NZZ we only group if we have 5 digits numbers
