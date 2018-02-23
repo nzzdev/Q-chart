adc9d6cf4710537db0add8d55b0690e920155cc3
benib <beni.buess@nzz.ch>
2018-02-23 14:42:38 +0100

    cleanup

--- routes/rendering-info/web.js
+++ routes/rendering-info/web.js
@@ -111,12 +111,6 @@ module.exports = {
       const functionName = `loadSVG${context.id}`;
       const dataObject = `${context.id}Data`;
 
-      const queryParams = {};
-      // add the item id to appendItemToPayload if it's state is in the db (aka not preview)
-      if (request.payload.itemStateInDb) {
-        queryParams.appendItemToPayload = request.query._id;
-      }
-
       let toolRuntimeConfigForWebSVG = {};
 
       // we only want to send the vega spec if we need it
@@ -132,12 +126,14 @@ module.exports = {
       let requestMethod;
       let requestBodyString;
 
+      const queryParams = {};
       // if we have the current item state in DB, we do a GET request, otherwise POST with the item and toolRuntimeConfig in the payload
       if (request.payload.itemStateInDb === true) {
         requestMethod = "GET";
         queryParams.toolRuntimeConfig = JSON.stringify(
           toolRuntimeConfigForWebSVG
         );
+        // add the item id to appendItemToPayload if it's state is in the db (aka not preview)
         queryParams.appendItemToPayload = request.query._id;
       } else {
         requestMethod = "POST";
