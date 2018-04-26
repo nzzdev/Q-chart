0213780eb413ecd0ebd179f1de1b57d6736e2d66
benib <beni.buess@nzz.ch>
2018-04-26 17:10:47 +0200

    (fixtures) add more bar/column fixtures

--- routes/fixtures/data.js
+++ routes/fixtures/data.js
@@ -3,9 +3,13 @@ const fixtureDataDirectory = "../../resources/fixtures/data";
 // provide every fixture data file present in ../../resources/fixtures/data
 // has to be in sync with files created in build task - see ../../tasks/build.js
 const fixtureData = [
+  require(`${fixtureDataDirectory}/bar-dates-days-prognosis.json`),
   require(`${fixtureDataDirectory}/bar-dates-quarter.json`),
   require(`${fixtureDataDirectory}/bar-qualitative-negative.json`),
-  require(`${fixtureDataDirectory}/stacked-bar-qualitative-negative.json`)
+  require(`${fixtureDataDirectory}/bar-qualitative-single-serie.json`),
+  require(`${fixtureDataDirectory}/stacked-bar-days-prognosis.json`),
+  require(`${fixtureDataDirectory}/stacked-bar-qualitative-negative.json`),
+  require(`${fixtureDataDirectory}/vegaSpec.json`)
 ];
 
 module.exports = {
