c5ada48a5cd64cc61f63664cb6ffc44c40f4e735
benib <beni.buess@nzz.ch>
2018-06-27 15:40:07 +0200

    (notification check) fix unsupportedNotificationCheck

--- routes/notification/unsupportedDateFormat.js
+++ routes/notification/unsupportedDateFormat.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 const helpers = require("../../helpers/dateSeries.js");
 
-const separators = [",", "/", "-"];
+const separators = [".", "/", "-"];
 
 module.exports = {
   method: "POST",
@@ -27,26 +27,25 @@ module.exports = {
       }
 
       // a wrong date format is detected by strings with two dots or slashes or dashes separating numbers
-      for (let row of data.slice(1)) {
-        for (let separator of separators) {
+      for (let separator of separators) {
+        for (let row of data.slice(1)) {
           let parts = row[0].split(separator);
           if (
-            parts.length !== 3 &&
-            Number.isNaN(parseInt(parts[0])) &&
-            Number.isNaN(parseInt(parts[1])) &&
-            Number.isNaN(parseInt(parts[2]))
+            parts.length === 3 &&
+            !Number.isNaN(parseInt(parts[0])) &&
+            !Number.isNaN(parseInt(parts[1])) &&
+            !Number.isNaN(parseInt(parts[2]))
           ) {
-            return null;
+            return {
+              message: {
+                title: "notifications.unsupportedDateFormat.title",
+                body: "notifications.unsupportedDateFormat.body"
+              }
+            };
           }
         }
       }
-
-      return {
-        message: {
-          title: "notifications.unsupportedDateFormat.title",
-          body: "notifications.unsupportedDateFormat.body"
-        }
-      };
+      return null;
     } catch (err) {
       return null;
     }
