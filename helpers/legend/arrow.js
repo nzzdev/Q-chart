633f2ba568a6e155b268c55554aeb22210916552
benib <beni.buess@nzz.ch>
2018-07-23 20:11:41 +0200

    (chore) prettier code formatting

--- helpers/legend/arrow.js
+++ helpers/legend/arrow.js
@@ -1,10 +1,12 @@
-const configuredDivergingColorSchemes = require('../colorSchemes.js').getConfiguredDivergingColorSchemes();
-const vega = require('vega');
+const configuredDivergingColorSchemes = require("../colorSchemes.js").getConfiguredDivergingColorSchemes();
+const vega = require("vega");
 
 function getColorSchemeName(item) {
   let itemColorScheme;
   try {
-    itemColorScheme = configuredDivergingColorSchemes[item.options.arrowOptions.colorScheme].scheme_name;
+    itemColorScheme =
+      configuredDivergingColorSchemes[item.options.arrowOptions.colorScheme]
+        .scheme_name;
   } catch (e) {
     itemColorScheme = null;
   }
@@ -12,17 +14,21 @@ function getColorSchemeName(item) {
 }
 
 function hasOnlyPositiveChanges(item) {
-  for (let row of item.data.slice(1)) { // loop over rows except header
-    if (row[1] > row[2]) {              // if the first value is > than the 2nd, we do not have only positive changes
+  for (let row of item.data.slice(1)) {
+    // loop over rows except header
+    if (row[1] > row[2]) {
+      // if the first value is > than the 2nd, we do not have only positive changes
       return false;
     }
   }
   return true;
 }
 
-function hasOnlyNegativeChanges(item) {
-  for (let row of item.data.slice(1)) { // loop over rows except header
-    if (row[2] > row[1]) {              // if the 2nd value is > than the first, we do not have only positive changes
+function hasOnlyNegativeOrZeroChanges(item) {
+  for (let row of item.data.slice(1)) {
+    // loop over rows except header
+    if (row[2] > row[1]) {
+      // if the 2nd value is > than the first, we do not have only positive changes
       return false;
     }
   }
@@ -35,8 +41,8 @@ function getLegendModel(item, toolRuntimeConfig) {
     return null;
   }
 
-  let arrowColor = '#6E6E7E'; // the default gray
-  let arrowTranslate = '';
+  let arrowColor = "#6E6E7E"; // the default gray
+  let arrowTranslate = "";
   let firstLabel = item.data[0][1];
   let lastLabel = item.data[0][2];
 
@@ -44,15 +50,17 @@ function getLegendModel(item, toolRuntimeConfig) {
 
   if (hasOnlyPositiveChanges(item)) {
     if (colorSchemeName) {
-      arrowColor = toolRuntimeConfig.colorSchemes[getColorSchemeName(item)][3][2];
+      arrowColor =
+        toolRuntimeConfig.colorSchemes[getColorSchemeName(item)][3][2];
     } else {
-      arrowColor = vega.schemeDiscretized('redblue')[3][2];
+      arrowColor = vega.schemeDiscretized("redblue")[3][2];
     }
-  } else if (hasOnlyNegativeChanges(item)) {
+  } else if (hasOnlyNegativeOrZeroChanges(item)) {
     if (colorSchemeName) {
-      arrowColor = toolRuntimeConfig.colorSchemes[getColorSchemeName(item)][3][0];
+      arrowColor =
+        toolRuntimeConfig.colorSchemes[getColorSchemeName(item)][3][0];
     } else {
-      arrowColor = vega.schemeDiscretized('redblue')[3][0];
+      arrowColor = vega.schemeDiscretized("redblue")[3][0];
     }
     // rotate the arrow to point to the left
     arrowTranslate = "rotate(180 14.5 5.5)";
@@ -60,7 +68,7 @@ function getLegendModel(item, toolRuntimeConfig) {
     firstLabel = item.data[0][1];
     lastLabel = item.data[0][2];
   }
-  
+
   return {
     legendItems: [
       {
