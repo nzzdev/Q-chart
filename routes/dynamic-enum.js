d63358307fe745af8b2e95dd2a9edc2f46f171eb
Manuel Roth <manuelroth@hotmail.ch>
2018-10-25 14:03:24 +0200

    (dynamicEnums) Convert dynamicEnums schema to use new format

--- routes/dynamic-enum.js
+++ routes/dynamic-enum.js
@@ -1,16 +1,17 @@
-const Boom = require('boom');
-const Joi = require('joi');
-const getFirstColumnSerie = require('../helpers/dateSeries.js').getFirstColumnSerie;
+const Boom = require("boom");
+const Joi = require("joi");
+const getFirstColumnSerie = require("../helpers/dateSeries.js")
+  .getFirstColumnSerie;
 
 function getChartTypeEnumWithTitles(item) {
   const chartTypes = {
     enum: ["Bar", "StackedBar", "Line", "Dotplot"],
     enum_titles: ["Säulen", "Gestapelte Säulen", "Linien", "Dot Plot"]
-  }
+  };
   try {
     if (item.data[0].length === 3) {
-      chartTypes.enum.push('Arrow');
-      chartTypes.enum_titles.push('Pfeile');
+      chartTypes.enum.push("Arrow");
+      chartTypes.enum_titles.push("Pfeile");
     }
   } catch (e) {
     // ignore the error, arrow will only be available if we have exactly 3 columns
@@ -28,30 +29,30 @@ function getHighlightEnum(item) {
 
 function getHighlightEnumTitles(item) {
   if (item.data.length < 1) {
-    return ['keine'];
+    return ["keine"];
   }
-  return ['keine'].concat(item.data[0].slice(1));
-} 
+  return ["keine"].concat(item.data[0].slice(1));
+}
 
 function getPrognosisStartEnum(item) {
   try {
     // constructs an array like [null,0,1,2,3,...] with the indexes from the first data column
     return [null].concat([...getFirstColumnSerie(item.data).keys()]);
   } catch (e) {
-    return [null]
+    return [null];
   }
 }
 function getPrognosisStartEnumTitles(item) {
   try {
-    return ['keine'].concat(getFirstColumnSerie(item.data));
+    return ["keine"].concat(getFirstColumnSerie(item.data));
   } catch (e) {
-    return ['keine']
+    return ["keine"];
   }
 }
 
 module.exports = {
-  method: 'POST',
-  path:'/dynamic-enum/{optionName}',
+  method: "POST",
+  path: "/dynamic-enum/{optionName}",
   options: {
     validate: {
       payload: Joi.object()
@@ -59,24 +60,25 @@ module.exports = {
     cors: true
   },
   handler: function(request, h) {
-    if (request.params.optionName === 'chartType') {
-      return getChartTypeEnumWithTitles(request.payload);
+    const item = request.payload.item;
+    if (request.params.optionName === "chartType") {
+      return getChartTypeEnumWithTitles(item);
     }
 
-    if (request.params.optionName === 'highlightDataSeries') {
+    if (request.params.optionName === "highlightDataSeries") {
       return {
-        enum: getHighlightEnum(request.payload),
-        enum_titles: getHighlightEnumTitles(request.payload)
+        enum: getHighlightEnum(item),
+        enum_titles: getHighlightEnumTitles(item)
       };
     }
 
-    if (request.params.optionName === 'prognosisStart') {
+    if (request.params.optionName === "prognosisStart") {
       return {
-        enum: getPrognosisStartEnum(request.payload),
-        enum_titles: getPrognosisStartEnumTitles(request.payload)
+        enum: getPrognosisStartEnum(item),
+        enum_titles: getPrognosisStartEnumTitles(item)
       };
     }
 
     return Boom.badRequest();
   }
-}
+};
