a3749e3e764c6a2d433fa810ccfef642d57ff4a8
benib <beni.buess@nzz.ch>
2019-10-18 17:53:24 +0200

    (line) draw the highlighted lines in front

--- chartTypes/line/postprocessings.js
+++ chartTypes/line/postprocessings.js
@@ -1,28 +1,8 @@
 const commonPostprocessings = require("../commonPostprocessings.js");
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-
-function reverseLineOrder(svg, spec, item, toolRuntimeConfig) {
-  const svgDom = new JSDOM(svg);
-  const document = svgDom.window.document;
-
-  const dataSeriesGroup = document.querySelector(".data-series");
-
-  // this reverses the order of the line groups
-  // putting the first on last and thus to the front
-  for (const dataSerieElement of dataSeriesGroup.children) {
-    dataSeriesGroup.appendChild(dataSerieElement);
-  }
-
-  return document.body.innerHTML;
-}
 
 module.exports = [
   commonPostprocessings.hideRepeatingTickLabels,
   commonPostprocessings.hideTicksWithoutLabels,
   commonPostprocessings.highlightZeroGridLineIfPositiveAndNegative,
-  commonPostprocessings.addOutlineToAnnotationLabels,
-  {
-    process: reverseLineOrder
-  }
+  commonPostprocessings.addOutlineToAnnotationLabels
 ];
