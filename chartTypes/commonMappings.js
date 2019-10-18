c7676470a3711607b8acb7572dad5d41112dccf8
benib <beni.buess@nzz.ch>
2019-10-18 12:43:08 +0200

    (date series) fix prognosis for bars when dates are entered reversed (last date first)

--- chartTypes/commonMappings.js
+++ chartTypes/commonMappings.js
@@ -205,10 +205,13 @@ function getBarPrognosisMappings() {
         if (!Number.isInteger(prognosisStart)) {
           return;
         }
-        // add the signal with prognosisStart value
+        // add the signal
         objectPath.push(spec, "signals", {
-          name: "prognosisStart",
-          value: prognosisStart
+          name: "prognosisStartDate",
+          value: dateSeries.getPrognosisStartDate(
+            mappingData.originalItemData,
+            prognosisStart
+          )
         });
 
         // add the data for prognosis
@@ -218,7 +221,8 @@ function getBarPrognosisMappings() {
           transform: [
             {
               type: "filter",
-              expr: "datum.xIndex >= prognosisStart"
+              expr:
+                "timeFormat(datum.xValue, '%Q') >= timeFormat(prognosisStartDate, '%Q')"
             }
           ]
         });
