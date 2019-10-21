195d636c0e0ca9b76f987e1ef534b8d0c50ed437
benib <beni.buess@nzz.ch>
2019-10-21 17:55:33 +0200

    (lines) fix order of lines in svg

--- chartTypes/line/postprocessings.js
+++ chartTypes/line/postprocessings.js
@@ -1,8 +1,29 @@
 const commonPostprocessings = require("../commonPostprocessings.js");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+function reverseLineOrder(svg, spec, item, toolRuntimeConfig) {
+  const svgDom = new JSDOM(svg);
+  const document = svgDom.window.document;
+  const dataSeriesGroup = document.querySelector(".data-series");
+  // this reverses the order of the line groups
+  // putting the first on last and thus to the front
+  const lineGroups = Array.from(dataSeriesGroup.children).reverse();
+
+  // todo: we should put the highlighted lines here to the end of the array
+
+  for (const dataSerieElement of lineGroups) {
+    dataSeriesGroup.appendChild(dataSerieElement);
+  }
+  return document.body.innerHTML;
+}
 
 module.exports = [
   commonPostprocessings.hideRepeatingTickLabels,
   commonPostprocessings.hideTicksWithoutLabels,
   commonPostprocessings.highlightZeroGridLineIfPositiveAndNegative,
-  commonPostprocessings.addOutlineToAnnotationLabels
+  commonPostprocessings.addOutlineToAnnotationLabels,
+  {
+    process: reverseLineOrder
+  }
 ];
