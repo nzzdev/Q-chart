444aad772aed1df379c814ccc0f885e88bb2e700
fgervasi <fgervasi12@gmail.ch>
2021-03-01 11:49:28 +0100

    Fix label in middle of bar chart

--- chartTypes/bar/mapping.js
+++ chartTypes/bar/mapping.js
@@ -190,7 +190,7 @@ module.exports = function getMapping() {
                 field: "y"
               },
               dy: {
-                signal: "barWidth / 2"
+                signal: "(barWidth / 2) + 0.5", // +0.5 Fix font is not exact in the middle of bar
               },
               baseline: {
                 value: "middle"
